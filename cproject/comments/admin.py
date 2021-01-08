c10d46974ca0df1b0ae32bad96d26b8047630e43
Rybak-a87 <rybak.a87@gmail.com>
2021-01-09 01:35:06 +0200

    lesson-2

--- cproject/comments/admin.py
+++ cproject/comments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ("author", "title", "text")
+
+
+admin.site.register(Comment)
--- cproject/comments/models.py
+++ cproject/comments/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="Автор", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name="Название поста")
+    text = models.TextField(verbose_name="Текст")
+    comments = GenericRelation("comment")    # привязка коментариев (в shell - post.comments.all() - выведет коментарии к посту)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -10,8 +21,10 @@ class Comment(models.Model):
         "self", verbose_name="Родительский коментарий", on_delete=models.CASCADE,
         blank=True, null=True, related_name="comment_children"
     )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)    # для привязки коментариев к чему угодно
     object_id = models.PositiveIntegerField()
+
     time_stamp = models.DateTimeField(auto_now=True, verbose_name="Дата создания комментария")
     ''' модно добавить лайки, дизрайки и т.д. '''
 
