23bd3ef652d244586e1e05cbd9d79de90146d0fc
Rybak-a87 <rybak.a87@gmail.com>
2021-01-12 23:01:31 +0200

    lesson-5

--- cproject/comments/templatetags/comments_tree.py
+++ cproject/comments/templatetags/comments_tree.py
@@ -1,9 +1,24 @@
 from django.template import Library
+from django.utils.html import mark_safe
 
 
 register = Library()
 
 
 @register.filter
-def comments_filter(obj):
-    pass
+def comments_filter(comments_list):
+    res = """
+            <ul>
+                {}
+            </ul>
+          """
+    i = ""
+    for comment in comments_list:
+        i += """
+                <li>
+                    {comment_id}
+                </li>
+             """.format(comment_id=comment["id"])
+        if comment.get("children"):    # для отображения вложенности
+            i += comments_filter(comment["children"])
+    return mark_safe(res.format(i))
--- cproject/comments/views.py
+++ cproject/comments/views.py
@@ -7,7 +7,4 @@ from .utils import create_comments_tree
 def base_view(request):
     comments = Post.objects.first().comments.all()    # вызов комментариев поста
     result = create_comments_tree(comments)
-    for res in result:
-        print(res)
-        print("-----------")
-    return render(request, "base/base.html", {"comments": comments})
+    return render(request, "base/base.html", {"comments": result})
